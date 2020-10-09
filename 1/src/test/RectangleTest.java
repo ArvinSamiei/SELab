2c7f786c45470399974e83be45278ea8e41434d2
ArvinSamiei <samiei.arvin@gmail.com>
2020-10-09 20:57:01 +0330

    dip and ocp principles

--- 1/src/test/RectangleTest.java
+++ 1/src/test/RectangleTest.java
@@ -3,13 +3,14 @@ package test;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class ReactangleTest {
+public class RectangleTest {
 
     @Test
     public void computeArea(){
-        Rectangle rectangle = new Rectangle(10, 20);
+        Shape rectangle = new Rectangle(10, 20);
         int computedArea = rectangle.computeArea();
         Assert.assertEquals(200, computedArea);
     }
 
 }
+
